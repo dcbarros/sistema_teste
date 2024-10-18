9ca81ce40394bdf12b26a6c55422ca739e24c632
dcbarros <dcbarros1@gmail.com>
2024-10-18 13:15:12 -0300

    feat: add swagger dependencies

--- src/main/java/com/api/teste/demo/DemoApplication.java
+++ src/main/java/com/api/teste/demo/DemoApplication.java
@@ -3,7 +3,19 @@ package com.api.teste.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "API ensino integração com bancos",
+		version = "1.0",
+		description = "Documentando uma API básica de gerenciamento de pedidos",
+		contact = @Contact(name = "Davison", email = "dcbarros11@gmail.com", url = "https://www.github.com/dcbarros")
+	)
+)
 public class DemoApplication {
 
 	public static void main(String[] args) {
